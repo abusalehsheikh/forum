64b4d76074197f5bb61aa0bcd2dfcb68f50a4f0b
Abu Saleh <abusalehpro@gmail.com>
2019-06-28 23:34:32 +0600

    JWT Package

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,18 @@ Route::apiResource('/category','CategoryController');
 Route::apiResource('/question/{question}/reply','ReplyController');
 Route::post('/like/{reply}','LikeController@like');
 Route::delete('/like/{reply}','LikeController@unlike');
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
