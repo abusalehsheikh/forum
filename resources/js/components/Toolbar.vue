54a940aab27a5d67f923caab0de8415c56cada5a
Abu Saleh <abusalehpro@gmail.com>
2019-06-30 01:07:29 +0600

    Login component

--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -1,13 +1,19 @@
 <template>
     <v-toolbar>
 <!--        <v-toolbar-side-icon></v-toolbar-side-icon>-->
-        <v-toolbar-title>Home</v-toolbar-title>
+        <v-toolbar-title>
+            <v-btn to="/">Home</v-btn>
+        </v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
+
             <v-btn flat>Forum</v-btn>
             <v-btn flat>Ask Question</v-btn>
             <v-btn flat>Category</v-btn>
-            <v-btn flat>login</v-btn>
+            <v-btn flat>
+                <router-link to="/login">Login</router-link>
+            </v-btn>
+
         </v-toolbar-items>
     </v-toolbar>
 </template>
---
+++ resources/js/components/login/Login.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-container>
+    <form @submit.prevent="login">
+
+        <v-text-field
+                v-model="form.email"
+                label="E-mail"
+                required
+                type="email"
+        ></v-text-field>
+
+        <v-text-field
+                v-model="form.password"
+                label="Password"
+                required
+                type="password"
+        ></v-text-field>
+
+
+
+        <v-btn type="submit" color="green">Login</v-btn>
+    </form>
+    </v-container>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                form :{
+                    email:null,
+                    password:null
+                }
+            }
+        },
+        methods:{
+            login(){
+                axios.post('api/auth/login',this.form)
+                    .then(res => console.log(res.data))
+                    .catch(error => console.log(error.response.data))
+            }
+        }
+
+    }
+</script>
+<style></style>
